42e303d9347b40cdf7fff730f05c6c6e6d7a661f
MmeHarmand <140753824+MmeHarmand@users.noreply.github.com>
2023-08-07 11:54:24 +0000

    modified:   Calculatrice/cal.js

--- Calculatrice/cal.js
+++ Calculatrice/cal.js
@@ -1,14 +1,29 @@
-function calculator(){
+function Calculator(){
     that = this;
     this.field= "inputnumber",
     this.button= "#body #buttons",
     this.init= false,
 
     this.run= function(){
+        $(this.button).click(function(){
+            var value = $(this).html();
+
+            if (that. init == false)
+            {
+                $(that.field).val("");
+                that.init= true;
+            }
+
+            if (value != "=")
+
+            $(that.field).val($(that.field).val() + value);
+
+            that.dispatcher(value);
+        });
 
     },
 
-    this.dipatcher= function(value){
+    this.dispatcher= function(value){
 
     }
 }
