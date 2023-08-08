3bb726f9c122018aa08865b18b025cf372356590
MmeHarmand <140753824+MmeHarmand@users.noreply.github.com>
2023-08-08 20:36:03 +0000

    new file:   Background/FondBois.jpg 	modified:   MurFond/MurFond.css 	modified:   MurFond/MurFond.html 	modified:   MurFond/MurFond.js

--- MurFond/MurFond.js
+++ MurFond/MurFond.js
@@ -3,5 +3,21 @@ function changePage(subject){
 }
 
 function codeShow(){
-    document.getElementById('code').style.display="block";
+    document.getElementById('container').style.display="block";
+}
+
+function clearScreen(){
+    document.getElementById("number").value = "";
+}
+
+function display(value) {
+    document.getElementById("number").value += value;
+}
+
+function correct(answer){
+    var ok = document.getElementById('number').value;
+    if(ok == answer){
+        document.getElementById("container").style.display= "none";
+        document.getElementById("good").style.display= "block";
+    }
 }
