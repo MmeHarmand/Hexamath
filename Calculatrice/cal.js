86e4a5d4232e33a265074fd02bb772f380dbcaeb
MmeHarmand <140753824+MmeHarmand@users.noreply.github.com>
2023-08-08 09:59:52 +0000

    modified:   Calculatrice/cal.css 	modified:   Calculatrice/cal.js 	modified:   Calculatrice/index.html

--- Calculatrice/cal.js
+++ Calculatrice/cal.js
@@ -1,46 +1,11 @@
-/*function Calculator() {
-    that = this;
-    //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-    this.t = document.getElementsByClassName("buttons");
-    // this.field = "inputnumber",
-    //     this.button = "#body #buttons",
-    //     this.init = false,
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
 
-    this.run = function () {
-        console.log(t);
+// https://www.w3schools.com/jsref/event_onload.asp
 
-        // $(this.button).click(function(){
-        //     var value = $(this).html();
-
-        //     if (that. init == false)
-        //     {
-        //         $(that.field).val("");
-        //         that.init= true;
-        //     }
-
-        //     if (value != "=")
-
-        //     $(that.field).val($(that.field).val() + value);
-
-        //     that.dispatcher(value);
-        // });
-
-    },
-
-        this.dispatcher = function (value) {
-
-        }
-}*/
-
-function removeZero(){
-    var value = document.getElementById("number").innerHTML;
-    if (value == "0"){
-        value = " "
-        document.getElementById("number").innerHTML = value;
-    }
+function clearScreen(){
+    document.getElementById("number").value = "";
 }
 
-function fordisplay(value){
-    removeZero()
-    document.getElementById("number").innerHTML += value;
+function display(value) {
+    document.getElementById("number").value += value;
 }
