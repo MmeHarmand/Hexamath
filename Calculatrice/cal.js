cdb0416ab870c0f02cda9bd435e1fd0427ea5e06
MmeHarmand <140753824+MmeHarmand@users.noreply.github.com>
2023-08-08 19:02:03 +0000

    modified:   Calculatrice/cal.css 	modified:   Calculatrice/cal.js 	modified:   Calculatrice/index.html

--- Calculatrice/cal.js
+++ Calculatrice/cal.js
@@ -9,3 +9,11 @@ function clearScreen(){
 function display(value) {
     document.getElementById("number").value += value;
 }
+
+function correct(answer){
+    var ok = document.getElementById('number').value;
+    if(ok == answer){
+        document.getElementById("container").style.display= "none";
+        document.getElementById("good").style.display= "block";
+    }
+}
